dfbcfff6e85cf8500ad7e9ca1af53a472d58ec9e
Tashfiq Rahman <31194800+tashfiq12@users.noreply.github.com>
2020-11-09 04:24:35 -0800

    Update index.js
    integrated some changes

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const writeStream = fs.createWriteStream('Notices_MIST.csv');
-writeStream.write(`Date,Notice \n`);
-writeStream.write(`\n\n`);
+const request = require('request')
+const cheerio = require('cheerio')
+const fs = require('fs')
+const url="https://mist.ac.bd"
+const writeStream = fs.createWriteStream('Notices_MIST.csv')
+writeStream.write(`Date,Title,Link \n`)
+writeStream.write(`\n\n`)
 
-request('https://mist.ac.bd', (error, response, html) => {
+request(url, (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
-    var noticearraylist = [];
-    let noticeMap = new Map();
+    var noticearraylist = []
+    let noticeMap = new Map()
 
-    $('article.event').each((i, el) => {
-      const notticetitle = $(el)
-        .find('h3.event-title')
-        .text()
-        .trim();
-      const noticeday = $(el)
-        .find('a.date')
-        .text();
-      var noticeobject = new Object();
-      noticeobject.notticetitle = notticetitle;
-      noticeobject.noticedate = noticeday;
+    $('div .spost').each((i, el) => {
+      const notticetitle = $(el) .find('h4 a').text().trim()
+      const noticelink=url+($(el).find('h4 a').attr('href'))
+      const noticedate= ($(el).find('.event_date .month').text()) +" "+ ($(el).find('.event_date .date').text())
+
+      console.log(notticetitle+"  "+noticelink+"  "+noticedate)
+      var noticeobject = new Object()
+      noticeobject.noticedate = noticedate
+      noticeobject.notticetitle = notticetitle
+      noticeobject.noticelink= noticelink
       if (noticeMap.get(notticetitle) !== 1) {
-        noticearraylist.push(noticeobject);
-        noticeMap.set(notticetitle, 1);
+        noticearraylist.push(noticeobject)
+        noticeMap.set(notticetitle, 1)
       }
     });
 
     for (let notice of noticearraylist) {
-      writeStream.write(`${notice.noticedate},${notice.notticetitle} \n`);
+      writeStream.write(`${notice.noticedate},${notice.notticetitle},${notice.noticelink} \n`);
     }
   }
 });
